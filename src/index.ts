17201498f7a33742e26bca510a7be6cfcfaaecf6
Jordan Matelsky <j6k4m8@gmail.com>
2026-02-13 22:03:27 -0500

    Add default header when missing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { lex } from "./lexer";
 import { parse } from "./parser";
 import { FileAST } from "./ast";
+export * from "./ast";
 
 export function parseCATL(input: string): FileAST {
     const { tokens, diagnostics } = lex(input);
